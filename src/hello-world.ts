107bc7382e13057bd8b3993ccf0d6c3fd3a41f1f
Tomas Valentukevičius <tomas.v@8devices.com>
2023-02-22 18:15:11 +0200

    Add peer dependency for AWS SDK

--- src/hello-world.ts
+++ src/hello-world.ts
@@ -1,6 +1,39 @@
+import { RDSDataClient } from "@aws-sdk/client-rds-data";
+
 export function sayHello() {
     console.log("hi");
 }
 export function sayGoodbye() {
     console.log("goodbye");
 }
+
+export class Crust {
+    constructor(private readonly rdsClient: RDSDataClient) {}
+}
+
+type ColumnTypeName =
+    | "BIGINT UNSIGNED"
+    | "BIT"
+    | "CHAR"
+    | "INT UNSIGNED"
+    | "JSON"
+    | "TIMESTAMP";
+
+type ColumnMetadata = {
+    arrayBaseColumnType: number;
+    isAutoIncrement: boolean;
+    isCaseSensitive: false;
+    isCurrency: false;
+    isSigned: false;
+    label: string;
+    name: string;
+    nullable: number;
+    precision: number;
+    scale: number;
+    schemaName: string;
+    tableName: string;
+    type: number;
+    typeName: ColumnTypeName;
+};
+
+type RecordType = "isNull" | "stringValue" | "longValue";
